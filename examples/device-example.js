79a84fed66e9351b22258451e3bbcae2c449d0ec
Starla Anderson <phil@philipleder.com>
2016-12-26 18:19:42 -0800

    back up to gpio

--- examples/device-example.js
+++ examples/device-example.js
@@ -24,7 +24,7 @@ const cmdLineProcess = require('./lib/cmdline');
 //begin module
 
 // Calling export with a pin number will export that header and return a gpio header instance
-var gpio4 = gpio.export(4, {
+var gpio8 = gpio.export(8, {
    // When you export a pin, the default direction is out. This allows you to set
    // the pin value to either LOW or HIGH (3.3V) from your program.
    direction: 'out',
@@ -37,8 +37,29 @@ var gpio4 = gpio.export(4, {
    // read or write to the header right away. Place your logic in this ready
    // function to guarantee everything will get fired properly
    ready: function() {
+      console.log("8 Ready")
    }
 });
+
+// Calling export with a pin number will export that header and return a gpio header instance
+var gpio24 = gpio.export(24, {
+   // When you export a pin, the default direction is out. This allows you to set
+   // the pin value to either LOW or HIGH (3.3V) from your program.
+   direction: 'out',
+
+   // set the time interval (ms) between each read when watching for value changes
+   // note: this is default to 100, setting value too low will cause high CPU usage
+   interval: 200,
+
+   // Due to the asynchronous nature of exporting a header, you may not be able to
+   // read or write to the header right away. Place your logic in this ready
+   // function to guarantee everything will get fired properly
+   ready: function() {
+      console.log("24 Ready")
+   }
+});
+
+gpio8.set();
 
 
 function processTest(args) {
