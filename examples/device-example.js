f0de522afc1ea712dd08347f20224303db5fda4b
Starla Anderson <phil@philipleder.com>
2016-12-26 18:16:54 -0800

    back up to gpio

--- examples/device-example.js
+++ examples/device-example.js
@@ -14,20 +14,7 @@
  */
 
 //node.js deps
-var raspi = require('raspi-io');
-var five = require('johnny-five');
-var board = new five.Board({
-   io: new raspi()
-});
-
-board.on('ready', function() {
-
-   // Create an Led on pin 7 (GPIO4) on P1 and strobe it on/off
-   // Optionally set the speed; defaults to 100ms
-   (new five.Led('P1-7')).strobe();
-
-});
-
+var gpio = require('gpio');
 //npm deps
 
 //app deps
@@ -35,6 +22,24 @@ const deviceModule = require('..').device;
 const cmdLineProcess = require('./lib/cmdline');
 
 //begin module
+
+// Calling export with a pin number will export that header and return a gpio header instance
+var gpio4 = gpio.export(4, {
+   // When you export a pin, the default direction is out. This allows you to set
+   // the pin value to either LOW or HIGH (3.3V) from your program.
+   direction: 'out',
+
+   // set the time interval (ms) between each read when watching for value changes
+   // note: this is default to 100, setting value too low will cause high CPU usage
+   interval: 200,
+
+   // Due to the asynchronous nature of exporting a header, you may not be able to
+   // read or write to the header right away. Place your logic in this ready
+   // function to guarantee everything will get fired properly
+   ready: function() {
+   }
+});
+
 
 function processTest(args) {
    //
